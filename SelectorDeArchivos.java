523f7a7f501b6bcf83e5ff362a7eaec359f893d1
LeirbagSouls <gabriel.prztz2003@gmail.com>
2024-06-17 01:05:28 -0600

    Update SelectorDeArchivos.java

--- SelectorDeArchivos.java
+++ SelectorDeArchivos.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package selectordearchivos;
 
-/**
- *
- * @author isabr
- */
 import javax.swing.*;
 import javax.swing.filechooser.FileFilter;
 import java.io.File;
