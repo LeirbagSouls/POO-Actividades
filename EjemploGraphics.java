9d67fac4990db0cf8ccfde5efb47293c7f09af2a
LeirbagSouls <gabriel.prztz2003@gmail.com>
2024-06-17 01:03:05 -0600

    Update EjemploGraphics.java

--- EjemploGraphics.java
+++ EjemploGraphics.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package ejemplographics;
 
-/**
- *
- * @author isabr
- */
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
