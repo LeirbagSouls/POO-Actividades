3404b66ee13f3423de80842bca5318be15ae59be
LeirbagSouls <gabriel.prztz2003@gmail.com>
2024-06-17 00:54:26 -0600

    Update DialogosModales.java

--- DialogosModales.java
+++ DialogosModales.java
@@ -1,13 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package dialogosmodales;
-
-/**
- *
- * @author isabr
- */
 import javax.swing.JOptionPane;
 
 public class DialogosModales {
