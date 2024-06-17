6b7d034736553fabfea42fe0495a7e42f10735c5
LeirbagSouls <gabriel.prztz2003@gmail.com>
2024-06-17 01:04:27 -0600

    Update Lampara.java

--- Lampara.java
+++ Lampara.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package lampara;
 
-/**
- *
- * @author isabr
- */
 import java.awt.*;
 import java.applet.Applet;
 import javax.swing.JFrame;
