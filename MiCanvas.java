698f3028dd17902563a3d48dcf3a3477e5e15acc
LeirbagSouls <gabriel.prztz2003@gmail.com>
2024-06-17 01:04:49 -0600

    Update MiCanvas.java

--- MiCanvas.java
+++ MiCanvas.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package micanvas;
 
-/**
- *
- * @author isabr
- */
 import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.Graphics;
