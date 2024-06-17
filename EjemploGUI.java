fcd4c5884ea4328b934c3229ffb8201c64d0fd4e
LeirbagSouls <gabriel.prztz2003@gmail.com>
2024-06-17 01:02:41 -0600

    Update EjemploGUI.java

--- EjemploGUI.java
+++ EjemploGUI.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package ejemplogui;
 
-/**
- *
- * @author isabr
- */
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
