70c50499558dd87cbdad5c30d32c5d440e9aeab9
LeirbagSouls <gabriel.prztz2003@gmail.com>
2024-06-17 00:53:12 -0600

    Update ConfirmacionDialogo.java

--- ConfirmacionDialogo.java
+++ ConfirmacionDialogo.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package confirmaciondialogo;
 
-/**
- *
- * @author isabr
- */
 import javax.swing.JOptionPane;
 
 public class ConfirmacionDialogo {
